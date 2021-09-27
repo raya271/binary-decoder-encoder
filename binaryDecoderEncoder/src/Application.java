5fd1859809fc2dd856c074a3e6ab640c032f6827
raya271 <chedatoge@gmail.com>
2021-09-27 18:23:07 +0200

    Konsoleneingabe funktioniert

--- binaryDecoderEncoder/src/Application.java
+++ binaryDecoderEncoder/src/Application.java
@@ -6,11 +6,22 @@ public class Application {
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        String binaerZahl = scanner.nextLine();
 
-        umrechner = new Umrechner();
-        int dezimalZahl = umrechner.decode(binaerZahl);
+        System.out.println("Um eine Binärzahl umzurechnen, gib eine 1 ein.");
+        System.out.println("Um eine Dezimalzahl umzurechnen, gib eine 2 ein.");
+        int programm = scanner.nextInt();
+        if (programm != 1 && programm != 2) {
+            System.out.println("Kein gültiges Programm ausgewählt!");
+            return;
+        }
+        System.out.println("Gib deine Zahl ein:");
+        int zahlEingabe = scanner.nextInt();
+        if (programm == 1){
+            zahlErgebnis = umrechner.decode(""+zahlEingabe);
+        } else {
+            zahlErgebnis = umrechner.encode(zahlEingabe);
+        }
+        System.out.println(zahlErgebnis);
 
-        System.out.println(dezimalZahl);
     }
 }
